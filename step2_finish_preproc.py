89547c79bb27e203e43bb7d082effc5f28d092a1
Nathan Muncy <nmuncy@fiu.edu>
2021-09-21 18:51:45 -0400

    finished skeleton

--- step2_finish_preproc.py
+++ step2_finish_preproc.py
@@ -54,48 +54,6 @@ def func_sbatch(command, wall_hours, mem_gig, num_proc, h_str, work_dir):
 
 
 # %%
-def func_scale(work_dir, phase_list, subj_num):
-
-    # """
-    # Step 10: Scale data
-
-    # 1) Data is scaled by mean signal
-    # """
-
-    for phase in phase_list:
-        epi_list = func_epi_list(phase, work_dir)
-        if not os.path.exists(os.path.join(work_dir, f"{phase}_minVal_mask+tlrc.HEAD")):
-            h_cmd = f"""
-                cd {work_dir}
-
-                3dMean \
-                    -datum short \
-                    -prefix tmp_mean_{phase} \
-                    tmp_run-{{1..{len(epi_list)}}}_{phase}_min+tlrc
-
-                3dcalc \
-                    -a tmp_mean_{phase}+tlrc \
-                    -expr 'step(a-0.999)' \
-                    -prefix {phase}_minVal_mask
-            """
-            func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}min", work_dir)
-
-    for phase in phase_list:
-        epi_list = func_epi_list(phase, work_dir)
-        for run in epi_list:
-            if not os.path.exists(os.path.join(work_dir, f"{run}_scale+tlrc.HEAD")):
-                h_cmd = f"""
-                    cd {work_dir}
-
-                    3dTstat -prefix tmp_tstat_{run} {run}_blur+tlrc
-
-                    3dcalc -a {run}_blur+tlrc \
-                        -b tmp_tstat_{run}+tlrc \
-                        -c {phase}_minVal_mask+tlrc \
-                        -expr 'c * min(200, a/b*100)*step(a)*step(b)' \
-                        -prefix {run}_scale
-                """
-                func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}scale", work_dir)
 
 
 # %%
@@ -107,7 +65,7 @@ def copy_data(prep_dir, work_dir, subj, sess, task, num_runs):
     func_str = f"{subj}_{sess}_task-{task}"
     copy_dict = {
         "anat": {
-            "tmp_struct_ns.nii.gz": f"{anat_str}_desc-preproc_T1w.nii.gz",
+            "tmp_struct_head.nii.gz": f"{anat_str}_desc-preproc_T1w.nii.gz",
             "tmp_struct_mask.nii.gz": f"{anat_str}_desc-brain_mask.nii.gz",
             "final_mask_GM_prob.nii.gz": f"{anat_str}_label-GM_probseg.nii.gz",
             "final_mask_WM_prob.nii.gz": f"{anat_str}_label-WM_probseg.nii.gz",
@@ -142,7 +100,7 @@ def copy_data(prep_dir, work_dir, subj, sess, task, num_runs):
         in_file = os.path.join(work_dir, tmp_file)
         h_str = tmp_file.split(".")[0].split("_", 1)[1]
         out_file = os.path.join(work_dir, f"{h_str}+tlrc")
-        if not os.path.exists(out_file):
+        if not os.path.exists(f"{out_file}.HEAD"):
             h_cmd = f"""
                 module load afni-20.2.06
                 3dcopy {in_file} {out_file} && rm {in_file}
@@ -194,7 +152,7 @@ def blur_epi(work_dir, subj_num, blur_mult):
 
 
 # %%
-def make_masks(work_dir, subj_num, atropos_dict, atropos_dir):
+def make_masks(work_dir, subj_num, tiss_list):
 
     epi_list = mk_epi_list(work_dir)
 
@@ -221,64 +179,95 @@ def make_masks(work_dir, subj_num, atropos_dict, atropos_dir):
                 -union \
                 -prefix tmp_mask_allRuns
 
-            # 3dresample \
-            #     -master tmp_mask_allRuns+tlrc \
-            #     -input struct_ns+tlrc \
-            #     -prefix tmp_anat_resamp
-
-            # 3dmask_tool \
-            #     -dilate_input 5 -5 \
-            #     -fill_holes \
-            #     -input tmp_anat_resamp+tlrc \
-            #     -prefix tmp_mask_struct
-
             3dmask_tool \
                 -input tmp_mask_allRuns+tlrc struct_mask+tlrc \
                 -inter \
                 -prefix mask_epi_anat
-
-            # 3dABoverlap \
-            #     -no_automask tmp_mask_allRuns+tlrc tmp_mask_struct+tlrc | \
-            #     tee out.mask_ae_overlap.txt
         """
         func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}uni", work_dir)
 
     # Make tissue-class masks
-    #   I like Atropos better than AFNI's way, so use those priors
-    h_ref = f"{epi_list[0]}_blur+tlrc"
-
-    for key in atropos_dict:
-        h_tiss = atropos_dict[key]
+    for tiss in tiss_list:
         if not os.path.exists(
-            os.path.join(work_dir, f"final_mask_{h_tiss}_eroded+tlrc.HEAD")
+            os.path.join(work_dir, f"final_mask_{tiss}_eroded+tlrc.HEAD")
         ):
             h_cmd = f"""
                 module load c3d-1.0.0-gcc-8.2.0
                 cd {work_dir}
 
                 c3d \
-                    {atropos_dir}/Prior{key}.nii.gz \
-                    -thresh 0.3 1 1 0 \
-                    -o tmp_{h_tiss}_bin.nii.gz
-
-                3dresample \
-                    -master {h_ref} \
-                    -rmode NN \
-                    -input tmp_{h_tiss}_bin.nii.gz \
-                    -prefix final_mask_{h_tiss}+tlrc
+                    final_mask_{tiss}_prob.nii.gz \
+                    -thresh 0.5 1 1 0 \
+                    -o tmp_{tiss}_bin.nii.gz
 
                 3dmask_tool \
-                    -input tmp_{h_tiss}_bin.nii.gz \
+                    -input tmp_{tiss}_bin.nii.gz \
                     -dilate_input -1 \
-                    -prefix tmp_mask_{h_tiss}_eroded
+                    -prefix final_mask_{tiss}_eroded
 
-                3dresample \
-                    -master {h_ref} \
-                    -rmode NN \
-                    -input tmp_mask_{h_tiss}_eroded+orig \
-                    -prefix final_mask_{h_tiss}_eroded
+                3drefit -space MNI final_mask_{tiss}_eroded+orig
+                3drefit -view tlrc final_mask_{tiss}_eroded+orig
             """
-            func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}atr", work_dir)
+            func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}tiss", work_dir)
+
+
+# %%
+def scale_epi(work_dir, subj_num, task):
+
+    epi_list = mk_epi_list(work_dir)
+
+    # TODO update here for multiple phases/tasks
+    for run in epi_list:
+        if not os.path.exists(os.path.join(work_dir, f"tmp_{run}_mask+tlrc.HEAD")):
+            h_cmd = f"""
+                module load afni-20.2.06
+                cd {work_dir}
+
+                3dcalc \
+                    -overwrite \
+                    -a {run}_preproc+tlrc \
+                    -expr 1 \
+                    -prefix tmp_{run}_mask
+
+                3dTstat \
+                    -min \
+                    -prefix tmp_{run}_min \
+                    tmp_{run}_mask+tlrc
+            """
+            h_min = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+            h_min.wait()
+
+    if not os.path.exists(os.path.join(work_dir, f"{task}_minVal_mask+tlrc.HEAD")):
+        h_cmd = f"""
+            cd {work_dir}
+
+            3dMean \
+                -datum short \
+                -prefix tmp_mean_{task} \
+                tmp_run-{{1..{len(epi_list)}}}_{task}_min+tlrc
+
+            3dcalc \
+                -a tmp_mean_{task}+tlrc \
+                -expr 'step(a-0.999)' \
+                -prefix {task}_minVal_mask
+        """
+        func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}min", work_dir)
+
+    # TODO update here for multiple phases/tasks
+    for run in epi_list:
+        if not os.path.exists(os.path.join(work_dir, f"{run}_scale+tlrc.HEAD")):
+            h_cmd = f"""
+                cd {work_dir}
+
+                3dTstat -prefix tmp_tstat_{run} {run}_blur+tlrc
+
+                3dcalc -a {run}_blur+tlrc \
+                    -b tmp_tstat_{run}+tlrc \
+                    -c {task}_minVal_mask+tlrc \
+                    -expr 'c * min(200, a/b*100)*step(a)*step(b)' \
+                    -prefix {run}_scale
+            """
+            func_sbatch(h_cmd, 1, 1, 1, f"{subj_num}scale", work_dir)
 
 
 # %%
@@ -299,7 +288,7 @@ def main():
         os.makedirs(work_dir)
 
     # get fMRIprep data
-    if not os.path.exists(os.path.join(work_dir, "struct_ns+tlrc.HEAD")):
+    if not os.path.exists(os.path.join(work_dir, "struct_head+tlrc.HEAD")):
         copy_data(prep_dir, work_dir, subj, sess, task, num_runs)
 
     # blur data
@@ -309,6 +298,15 @@ def main():
         blur_epi(work_dir, subj_num, blur_mult)
 
     # make subject intersection mask
+    tiss_list = ["GM", "WM"]
+    if not os.path.exists(
+        os.path.join(work_dir, f"final_mask_{tiss_list[0]}_eroded+tlrc.HEAD")
+    ):
+        make_masks(work_dir, subj_num, tiss_list)
+
+    # scale data
+    if not os.path.exists(os.path.join(work_dir, f"run-1_{task}_scale+tlrc.HEAD")):
+        scale_epi(work_dir, subj_num, task)
 
 
 if __name__ == "__main__":
