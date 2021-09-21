e2c3e3c85c0d104b23af84e083350f6053a04b6e
Nathan Muncy <nmuncy@fiu.edu>
2021-09-21 13:19:56 -0400

    cleared list to stop accumulation

--- step1_submit.sh
+++ step1_submit.sh
@@ -11,6 +11,7 @@ subj_all=(`ls $dset_dir | grep "sub-*"`)
 # Make a list of 8 subjects who don't have fMRIprep
 #   output in deriv_dir
 max_num=9
+unset subj_list
 for subj in ${subj_all[@]}; do
     check_file=${deriv_dir}/fmriprep/${subj}.html
     if [ ! -f $check_file ]; then
