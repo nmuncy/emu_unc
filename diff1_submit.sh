1f0b07cec84b5d64fa4ffb945441073032316f47
Nathan Muncy <nmuncy@fiu.edu>
2021-10-05 11:03:00 -0400

    attempting to resolve endpoint_info error

--- diff1_submit.sh
+++ diff1_submit.sh
@@ -3,10 +3,10 @@
 proj_dir=/scratch/madlab/emu_UNC
 diff_dir=${proj_dir}/derivatives/dwi_preproc
 
-# get json AFQ needs
-ref_file=${proj_dir}/dset/dataset_description.json
-cp $ref_file $proj_dir
-cp $ref_file $diff_dir
+# # get json AFQ needs
+# ref_file=${proj_dir}/dset/dataset_description.json
+# cp $ref_file $proj_dir
+# cp $ref_file $diff_dir
 
 # submit job
 python diff1_prob_setup.py $proj_dir $diff_dir
