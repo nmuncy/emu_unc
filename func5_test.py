33a364c7c87173ed0c681403e7eb51a175191d31
Nathan Muncy <nmuncy@fiu.edu>
2021-10-18 18:07:33 -0400

    added make_seed, check_coord

--- func5_test.py
+++ func5_test.py
@@ -13,6 +13,7 @@ from func2_finish_preproc import func_sbatch
 def make_gmInt_mask(
     subj_list, deriv_dir, sess, phase, tplflow_dir, tplflow_str, frac_value, group_dir
 ):
+    """ Make a gray matter * group intersection mask """
 
     # set ref file for resampling
     ref_file = os.path.join(deriv_dir, subj_list[1], sess, f"run-1_{phase}_scale+tlrc")
@@ -81,6 +82,8 @@ def make_gmInt_mask(
 
 # %%
 def get_subbrick(subj_file, beh):
+    """ Find sub-brick of beh """
+
     h_cmd = f"""
         module load afni-20.2.06
         3dinfo \
@@ -97,10 +100,11 @@ def get_subbrick(subj_file, beh):
 
 # %%
 def get_pars(df_group, subj):
+    """ Determine subj pars group tertile"""
+
     subj_num = int(subj.split("-")[-1])
     idx_subj = df_group.index[df_group["emu_study_id"] == subj_num]
     subj_pars = df_group.iloc[idx_subj]["pars_6"].item()
-
     if subj_pars <= 3.0:
         subj_pars_group = "Low"
     elif subj_pars > 3.0 and subj_pars <= 12.0:
@@ -109,13 +113,63 @@ def get_pars(df_group, subj):
         subj_pars_group = "High"
     else:
         subj_pars_group = np.nan
-
     return subj_pars_group
 
 
 # %%
+def make_seed(seed_name, coord_list, group_dir, ref_file):
+    """ Make seed from supplied coordinate """
+
+    h_cmd = f"""
+        module load afni-20.2.06
+        echo "{coord_list}" > tmp_coord
+        3dUndump \
+            -prefix {group_dir}/Check_{seed_name}.nii.gz \
+            -master {ref_file} \
+            -srad 0 \
+            -xyz tmp_coord
+        rm tmp_coord
+    """
+    h_seed = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+    h_seed.wait()
+
+
+# %%
+def check_coord(coord_dict, group_dir, subj_int_mask):
+    """ Return True if meaninful data exists at all checked coordinates """
+
+    for seed in coord_dict:
+        mask = os.path.join(group_dir, f"Check_{seed}.nii.gz")
+        if not os.path.exists(mask):
+            make_seed(seed, coord_dict[seed], group_dir, subj_int_mask)
+        h_cmd = f"""
+            module load afni-20.2.06
+            3dROIstats \
+                -quiet \
+                -mask {mask} \
+                {subj_int_mask} |
+                tr -d '[:space:]'
+        """
+        h_check = subprocess.Popen(h_cmd, shell=True, stdout=subprocess.PIPE)
+        h_check.wait()
+        vox_value = h_check.communicate()[0]
+        out_status = True if float(vox_value.decode("utf-*")) == 1.0 else False
+        if not out_status:
+            return out_status
+    return out_status
+
+
+# %%
 def group_analysis(
-    beh_list, glt_dict, subj_list, sess, phase, group_dir, deriv_dir, df_group
+    beh_list,
+    glt_dict,
+    subj_list,
+    sess,
+    phase,
+    group_dir,
+    deriv_dir,
+    df_group,
+    coord_dict,
 ):
 
     data_table = []
@@ -123,6 +177,13 @@ def group_analysis(
         subj_file = os.path.join(
             deriv_dir, subj, sess, f"{phase}_decon_stats_REML+tlrc"
         )
+
+        # check that data exists at desired coords
+        subj_int_mask = os.path.join(deriv_dir, subj, sess, "mask_epi_anat+tlrc")
+        data_exists = check_coord(coord_dict, group_dir, subj_int_mask)
+        if not data_exists:
+            continue
+
         subj_pars_group = get_pars(df_group, subj)
         for beh in beh_list:
             subj_brick = get_subbrick(subj_file, beh)
@@ -261,6 +322,8 @@ def main():
         # TODO pull this csv from SharePoint
         df_sum = pd.read_csv("emuR01_summary_latest.csv")
         df_group = df_sum[["emu_study_id", "pinf_random", "pars_6"]]
+
+        coord_dict = {"LAmg": "-24 -5 -29", "RAmg": "22 -3 -29"}
         # group_analysis(beh_dict, glt_dict, subj_list, sess, phase, group_dir, deriv_dir)
 
     """ get subj acf estimates """
