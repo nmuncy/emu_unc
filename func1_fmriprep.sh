be791acdf06c9612d050ca4e961179662399e65b
Nathan Muncy <nmuncy@fiu.edu>
2021-10-20 11:44:56 -0400

    added documentation to func1 scripts

--- func1_fmriprep.sh
+++ func1_fmriprep.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Receives 5 arguments from func1_submit.sh, runs
+# singularity version of fMRIprep.
+#
+# Notes:
+#   --skull-strip-template, --output-spaces are hardcoded
+#
+# Required Input:
+#   1: subject id (int)
+#   2: singularity image
+#   3: BIDs-structured project directory
+#   4: FreeSurfer license
+#   5: location of templatflow directory
+
+
 #SBATCH --qos pq_madlab
 #SBATCH --account iacc_madlab
 #SBATCH -p IB_44C_512G
@@ -10,13 +24,14 @@
 #SBATCH --job-name nm_fmriprep
 
 
+# set up
 module load singularity-3.8.2
 
-
-# receive args
 label=$1
 sing_img=$2
 proj_dir=$3
+fs_license=$4
+tpflow_dir=$5
 
 # set paths
 dset_dir=${proj_dir}/dset
@@ -25,8 +40,8 @@ work_dir=${deriv_dir}/tmp_work/sub-${label}
 mkdir -p $work_dir
 
 # reference template fow, fs, avoid root issues
-export SINGULARITYENV_TEMPLATEFLOW_HOME=/home/data/madlab/singularity-images/templateflow
-export FS_LICENSE=~/bin/licenses/fs_license.txt
+export SINGULARITYENV_TEMPLATEFLOW_HOME=$tpflow_dir
+export FS_LICENSE=$fs_license
 cd /
 
 # do job
