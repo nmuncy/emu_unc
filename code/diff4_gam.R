cbc55a918c467b6f6db3c02f8010d17181e8a997
Nathan Muncy <nmuncy@fiu.edu>
2022-02-03 17:34:09 -0500

    working on cing dist

--- code/diff4_gam.R
+++ code/diff4_gam.R
@@ -7,27 +7,21 @@ library("dplyr")
 library("mgcViz")
 
 
-# Set up paths ----
+# Set Up ----
 data_dir <- file_path_as_absolute(paste0(getwd(), "/../data"))
 # capture.output(sessionInfo(), file = paste0(data_dir, "R_session_info.txt"))
-
-
-# Determine data distribution, base GAM function ----
-#
-#
-
-# get data
 df_afq <- read.csv(paste0(data_dir, "/AFQ_dataframe.csv"))
+df_afq$sex <- factor(df_afq$sex)
+df_afq$subjectID <- factor(df_afq$subjectID)
 
-# tracts of interest
-tract_list <- c("UNC_L", "CGC_L")
+
+# L. Unc: Determine data distribution, base GAM function ----
+#
+#
 
 # subset df_afq, take complete cases, make factors
-tract <- "UNC_L"
-df_tract <- df_afq[which(df_afq$tractID == tract), ]
+df_tract <- df_afq[which(df_afq$tractID == "UNC_L"), ]
 df_tract <- df_tract[complete.cases(df_tract), ]
-df_tract$sex <- factor(df_tract$sex)
-df_tract$subjectID <- factor(df_tract$subjectID)
 
 # determine distribution
 hist(df_tract$dti_fa)
@@ -35,11 +29,11 @@ descdist(df_tract$dti_fa, discrete = F)
 
 # build gam
 fit_normal <- bam(dti_fa ~ sex +
-                    s(subjectID, bs = "re") +
-                    s(nodeID, bs = "cr", by = sex, k = 40),
-                  data = df_tract,
-                  family = gaussian(),
-                  method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", by = sex, k = 40),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(fit_normal, rep = 1000)
 summary(fit_normal)
@@ -51,12 +45,12 @@ plot(sm(plot_normal, 3))
 # get stat of male smooth diff from female
 df_tract$sexOF <- factor(df_tract$sex, ordered = T)
 fit_normalOF <- bam(dti_fa ~ sexOF +
-                      s(subjectID, bs = "re") +
-                      s(nodeID, bs = "cr", k = 40) +
-                      s(nodeID, by = sexOF, bs = "cr", k = 40),
-                    data = df_tract,
-                    family = gaussian(),
-                    method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 40) +
+  s(nodeID, by = sexOF, bs = "cr", k = 40),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 summary(fit_normalOF)
 plot(fit_normalOF)
@@ -67,12 +61,12 @@ report_stats(fit_normalOF)
 
 # add pds covariate
 fit_pds <- bam(dti_fa ~ sex +
-                 s(pds, by = sex) +
-                 s(subjectID, bs = "re") +
-                 s(nodeID, bs = "cr", k = 40),
-               data = df_tract,
-               family = gaussian(),
-               method = "fREML"
+  s(pds, by = sex) +
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 40),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(fit_pds, rep = 1000)
 compareML(fit_normal, fit_pds) # fit normal preferred
@@ -84,19 +78,18 @@ gamtabs(fit_pds)
 report_stats(fit_pds)
 
 
-
-# GAM with continuous Parent's SCARED ----
+# L. Unc: GAM with continuous Parent's SCARED ----
 #
 # Rather than looking for group differences, see if adding anxiety measure
 # changes model fit. Then plot those two splines to find differences.
 
 # intx of scared with fa
 fit_pscared_intx <- bam(dti_fa ~ sex +
-                     s(subjectID, bs = "re") +
-                     te(nodeID, pscared, bs = c("cr", "tp"), k = c(40, 10)),
-                   data = df_tract,
-                   family = gaussian(),
-                   method = "fREML"
+  s(subjectID, bs = "re") +
+  te(nodeID, pscared, bs = c("cr", "tp"), k = c(40, 10)),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(fit_pscared_intx, rep = 1000)
 k.check(fit_pscared_intx)
@@ -110,19 +103,25 @@ plot_pscared_intx <- getViz(fit_pscared_intx)
 plot(sm(plot_pscared_intx, 2))
 
 # 3d plot
-vis.gam(fit_pscared_intx, view=c("nodeID", "pscared"), color = "topo", theta=330, phi=40)
-vis.gam(fit_pscared_intx, view=c("nodeID", "pscared"), theta=60)
+vis.gam(
+  fit_pscared_intx, 
+  view = c("nodeID", "pscared"), 
+  color = "topo", 
+  theta = 330, 
+  phi = 40
+)
+vis.gam(fit_pscared_intx, view = c("nodeID", "pscared"), theta = 60)
 
 
 # decompose intx term
 fit_pscared_decomp <- bam(dti_fa ~ sex +
-                          s(subjectID, bs = "re") +
-                          s(nodeID, bs = "cr", k = 40) +
-                          s(pscared, k = 10) +
-                          ti(nodeID, pscared, bs = c("cr", "tp"), k = c(40, 10)),
-                        data = df_tract,
-                        family = gaussian(),
-                        method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 40) +
+  s(pscared, k = 10) +
+  ti(nodeID, pscared, bs = c("cr", "tp"), k = c(40, 10)),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(fit_pscared_decomp, rep = 1000)
 k.check(fit_pscared_decomp)
@@ -138,8 +137,8 @@ plot(sm(plot_pscared_decomp, 3))
 plot(sm(plot_pscared_decomp, 4))
 
 vis.gam(
-  fit_pscared_decomp, 
-  view = c("nodeID", "pscared"), 
+  fit_pscared_decomp,
+  view = c("nodeID", "pscared"),
   color = "topo",
   phi = 40,
   theta = 330
@@ -148,16 +147,17 @@ vis.gam(
 # vis.gam(fit_pscared_decomp, view = c("nodeID", "pscared"), plot.type = "contour", color = "topo", too.far = 0.05)
 
 
-# GAM with continuous PARS-6 ----
+# L. Unc: GAM with continuous PARS-6 ----
 #
 #
 
+# intx
 fit_pars6_intx <- bam(dti_fa ~ sex +
-                   s(subjectID, bs = "re") +
-                   te(nodeID, pars6, bs = c("cr", "tp"), k = c(40, 10)),
-                 data = df_tract,
-                 family = gaussian(),
-                 method = "fREML"
+  s(subjectID, bs = "re") +
+  te(nodeID, pars6, bs = c("cr", "tp"), k = c(40, 10)),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(fit_pars6_intx, rep = 1000)
 k.check(fit_pars6_intx)
@@ -168,15 +168,15 @@ compareML(fit_normal, fit_pars6_intx)
 plot_pars6_intx <- getViz(fit_pars6_intx)
 plot(sm(plot_pars6_intx, 2))
 
-
+# decompose
 fit_pars6_decomp <- bam(dti_fa ~ sex +
-                        s(subjectID, bs = "re") +
-                        s(nodeID, bs = "cr", k = 40) +
-                        s(pars6) +
-                        ti(nodeID, pars6, bs = c("cr", "tp"), k = c(40, 10)),
-                      data = df_tract,
-                      family = gaussian(),
-                      method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 40) +
+  s(pars6) +
+  ti(nodeID, pars6, bs = c("cr", "tp"), k = c(40, 10)),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(fit_pars6_decomp, rep = 1000)
 k.check(fit_pars6_decomp)
@@ -190,25 +190,24 @@ plot(sm(plot_pars6_decomp, 3))
 plot(sm(plot_pars6_decomp, 4))
 
 vis.gam(
-  fit_pars6_decomp, 
-  view = c("nodeID", "pars6"), 
+  fit_pars6_decomp,
+  view = c("nodeID", "pars6"),
   color = "topo",
   phi = 40,
   theta = 330
 )
 
 
-
-# GAM with continuous Child's SCARED ----
+# L. Unc: GAM with continuous Child's SCARED ----
 #
 
 # intx of scared with fa
 fit_cscared_intx <- bam(dti_fa ~ sex +
-                     s(subjectID, bs = "re") +
-                     te(nodeID, cscared, bs = c("cr", "tp"), k = c(40, 10)),
-                   data = df_tract,
-                   family = gaussian(),
-                   method = "fREML"
+  s(subjectID, bs = "re") +
+  te(nodeID, cscared, bs = c("cr", "tp"), k = c(40, 10)),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(fit_cscared_intx, rep = 1000)
 k.check(fit_cscared_intx)
@@ -222,19 +221,18 @@ plot_cscared_intx <- getViz(fit_cscared_intx)
 plot(sm(plot_cscared_intx, 2))
 
 # 3d plot
-vis.gam(fit_cscared_intx, view=c("nodeID", "cscared"), color = "topo", theta=330, phi=40)
-vis.gam(fit_cscared_intx, view=c("nodeID", "cscared"), theta=60)
-
+vis.gam(fit_cscared_intx, view = c("nodeID", "cscared"), color = "topo", theta = 330, phi = 40)
+vis.gam(fit_cscared_intx, view = c("nodeID", "cscared"), theta = 60)
 
 # parent's scared - decompose intx term
 fit_cscared_decomp <- bam(dti_fa ~ sex +
-                          s(subjectID, bs = "re") +
-                          s(nodeID, bs = "cr", k = 40) +
-                          s(cscared, k = 10) +
-                          ti(nodeID, cscared, bs = c("cr", "tp"), k = c(40, 10)),
-                        data = df_tract,
-                        family = gaussian(),
-                        method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 40) +
+  s(cscared, k = 10) +
+  ti(nodeID, cscared, bs = c("cr", "tp"), k = c(40, 10)),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(fit_cscared_decomp, rep = 1000)
 k.check(fit_cscared_decomp)
@@ -250,8 +248,8 @@ plot(sm(plot_cscared_decomp, 3))
 plot(sm(plot_cscared_decomp, 4))
 
 vis.gam(
-  fit_cscared_decomp, 
-  view = c("nodeID", "cscared"), 
+  fit_cscared_decomp,
+  view = c("nodeID", "cscared"),
   color = "topo",
   phi = 40,
   theta = 330
@@ -259,3 +257,102 @@ vis.gam(
 # vis.gam(fit_cscared_decomp, view = c("nodeID", "cscared"), plot.type = "contour", color = "topo", too.far = 0.1)
 # vis.gam(fit_cscared_decomp, view = c("nodeID", "cscared"), plot.type = "contour", color = "topo", too.far = 0.05)
 
+
+
+# L. Cing: Determine data distribution, base GAM function ----
+#
+#
+
+# subset df_afq, take complete cases, make factors
+df_tract <- df_afq[which(df_afq$tractID == "CGC_L"), ]
+df_tract <- df_tract[complete.cases(df_tract), ]
+
+# determine distribution
+hist(df_tract$dti_fa)
+descdist(df_tract$dti_fa, discrete = F)
+
+hist(log10(max(df_tract$dti_fa+1) - df_tract$dti_fa))
+descdist(log10(max(df_tract$dti_fa+1) - df_tract$dti_fa), discrete = F)
+
+
+# # build gam
+# fit_normal <- bam(dti_fa ~ 
+#                     s(subjectID, bs = "re") +
+#                     s(nodeID, bs = "cr", k = 40),
+#                   data = df_tract,
+#                   family = gaussian(),
+#                   method = "fREML"
+# )
+# gam.check(fit_normal, rep = 1000)
+# 
+# fit_beta <- bam(dti_fa ~ 
+#                     s(subjectID, bs = "re") +
+#                     s(nodeID, bs = "cr", k = 40),
+#                   data = df_tract,
+#                   family = betar(link = "logit"),
+#                   method = "fREML"
+# )
+# gam.check(fit_beta, rep = 1000)
+# compareML(fit_normal, fit_beta) # normal preferred
+# 
+# fit_gamma <- bam(dti_fa ~ 
+#                   s(subjectID, bs = "re") +
+#                   s(nodeID, bs = "cr", k = 40),
+#                 data = df_tract,
+#                 family = Gamma(link = "logit"),
+#                 method = "fREML"
+# )
+# gam.check(fit_gamma, rep = 1000)
+# compareML(fit_normal, fit_gamma) # normal preferred
+# 
+# fit_normal <- bam(dti_fa ~ sex +
+#                     s(subjectID, bs = "re") +
+#                     s(nodeID, by= sex, bs = "cr", k = 40),
+#                   data = df_tract,
+#                   family = gaussian(),
+#                   method = "fREML"
+# )
+# gam.check(fit_normal, rep = 1000)
+# plot(fit_normal)
+# 
+# 
+# summary(fit_normal)
+# plot_normal <- getViz(fit_normal)
+# plot(sm(plot_normal, 1))
+# plot(sm(plot_normal, 2))
+# plot(sm(plot_normal, 3))
+# 
+# # get stat of male smooth diff from female
+# df_tract$sexOF <- factor(df_tract$sex, ordered = T)
+# fit_normalOF <- bam(dti_fa ~ sexOF +
+#                       s(subjectID, bs = "re") +
+#                       s(nodeID, bs = "cr", k = 40) +
+#                       s(nodeID, by = sexOF, bs = "cr", k = 40),
+#                     data = df_tract,
+#                     family = gaussian(),
+#                     method = "fREML"
+# )
+# summary(fit_normalOF)
+# plot(fit_normalOF)
+# plot_normalOF <- getViz(fit_normalOF)
+# plot(sm(plot_normalOF, 3)) + geom_hline(yintercept = 0)
+# gamtabs(fit_normalOF)
+# report_stats(fit_normalOF)
+# 
+# # add pds covariate
+# fit_pds <- bam(dti_fa ~ sex +
+#                  s(pds, by = sex) +
+#                  s(subjectID, bs = "re") +
+#                  s(nodeID, bs = "cr", k = 40),
+#                data = df_tract,
+#                family = gaussian(),
+#                method = "fREML"
+# )
+# gam.check(fit_pds, rep = 1000)
+# compareML(fit_normal, fit_pds) # fit normal preferred
+# summary(fit_pds)
+# 
+# plot_pds <- getViz(fit_pds)
+# plot(sm(plot_pds, 4))
+# gamtabs(fit_pds)
+# report_stats(fit_pds)
