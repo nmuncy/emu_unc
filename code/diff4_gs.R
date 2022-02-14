c20768f1fd14cb6115ff04597a05f5467886e378
Nathan Muncy <nmuncy@fiu.edu>
2022-02-14 16:29:56 -0500

    more running in place

--- code/diff4_gs.R
+++ code/diff4_gs.R
@@ -53,63 +53,64 @@ ggplot(data=df_pred, aes(x=nodeID, y=dti_fa, group=subjectID)) +
   geom_line(aes(y=pred.fit), data=df_pred) +
   geom_point()
 
-# subset data
-sub_list <- seq(9, 89, by = 10)
-df_sub <- df_tract[seq(10, dim(df_tract)[1], by = 10),]
-df_sub <- df_sub[-which(df_sub$nodeID == 99),]
-
-lunc_GS <- bam(dti_fa ~ sex +
-                s(nodeID, bs = "cr", k = 9, m = 2) +
-                s(nodeID, subjectID, bs = "fs", k= 9, m = 2),
-              data = df_sub,
-              family = gaussian(),
-              method = "fREML",
-              discrete = T
-)
-gam.check(lunc_GS, rep = 1000)
-plot(lunc_GS)
-
-lunc_GS_pred <- predict(lunc_GS, se.fit=T)
-df_sub <- transform(df_sub, 
-                    modGS = lunc_GS_pred$fit,
-                    modGS_se= lunc_GS_pred$se.fit)
-
-ggplot(data=df_sub, aes(x=nodeID, y=dti_fa, group=subjectID)) +
-  facet_wrap(~subjectID) +
-  geom_ribbon(aes(ymin=modGS-2*modGS_se, 
-              ymax=modGS+2*modGS_se), alpha = 0.3) +
-  geom_line(aes(y=modGS)) +
-  geom_point()
-
-
-# full data
-lunc_GS <- bam(dti_fa ~ sex +
-                 s(nodeID, bs = "cr", k = 90, m = 2) +
-                 s(nodeID, subjectID, bs = "fs", k = 90, m = 2),
-               data = df_tract,
-               family = gaussian(),
-               method = "fREML",
-               discrete = T
-)
-gam.check(lunc_GS, rep = 1000)
-saveRDS(lunc_GS, file="/Users/nmuncy/Desktop/lunc_GS.rds")
-plot(lunc_GS)
-
-lunc_GS_pred <- predict(lunc_GS, se.fit=T)
-df_tract <- transform(df_tract, 
-                    modGS = lunc_GS_pred$fit,
-                    modGS_se= lunc_GS_pred$se.fit)
-
-ggplot(data=df_tract, aes(x=nodeID, y=dti_fa, group=subjectID)) +
-  facet_wrap(~subjectID) +
-  geom_ribbon(aes(ymin=modGS-2*modGS_se, 
-                  ymax=modGS+2*modGS_se), alpha = 0.3) +
-  geom_line(aes(y=modGS)) +
-  geom_point()
+# # subset data
+# sub_list <- seq(9, 89, by = 10)
+# df_sub <- df_tract[seq(10, dim(df_tract)[1], by = 10),]
+# df_sub <- df_sub[-which(df_sub$nodeID == 99),]
+# 
+# lunc_GS <- bam(dti_fa ~ sex +
+#                 s(nodeID, bs = "cr", k = 9, m = 2) +
+#                 s(nodeID, subjectID, bs = "fs", k= 9, m = 2),
+#               data = df_sub,
+#               family = gaussian(),
+#               method = "fREML",
+#               discrete = T
+# )
+# gam.check(lunc_GS, rep = 1000)
+# plot(lunc_GS)
+# 
+# lunc_GS_pred <- predict(lunc_GS, se.fit=T)
+# df_sub <- transform(df_sub, 
+#                     modGS = lunc_GS_pred$fit,
+#                     modGS_se= lunc_GS_pred$se.fit)
+# 
+# ggplot(data=df_sub, aes(x=nodeID, y=dti_fa, group=subjectID)) +
+#   facet_wrap(~subjectID) +
+#   geom_ribbon(aes(ymin=modGS-2*modGS_se, 
+#               ymax=modGS+2*modGS_se), alpha = 0.3) +
+#   geom_line(aes(y=modGS)) +
+#   geom_point()
+# 
+# 
+# # full data
+# lunc_GS <- bam(dti_fa ~ sex +
+#                  s(nodeID, bs = "cr", k = 90, m = 2) +
+#                  s(nodeID, subjectID, bs = "fs", k = 90, m = 2),
+#                data = df_tract,
+#                family = gaussian(),
+#                method = "fREML",
+#                discrete = T
+# )
+# gam.check(lunc_GS, rep = 1000)
+# saveRDS(lunc_GS, file="/Users/nmuncy/Desktop/lunc_GS.rds")
+# plot(lunc_GS)
+# 
+# lunc_GS_pred <- predict(lunc_GS, se.fit=T)
+# df_tract <- transform(df_tract, 
+#                     modGS = lunc_GS_pred$fit,
+#                     modGS_se= lunc_GS_pred$se.fit)
+# 
+# ggplot(data=df_tract, aes(x=nodeID, y=dti_fa, group=subjectID)) +
+#   facet_wrap(~subjectID) +
+#   geom_ribbon(aes(ymin=modGS-2*modGS_se, 
+#                   ymax=modGS+2*modGS_se), alpha = 0.3) +
+#   geom_line(aes(y=modGS)) +
+#   geom_point()
 
 
 # Smooth for group
 lunc_groupGS <- bam(dti_fa ~ sex +
+                 s(dx, bs = "re") +
                  s(subjectID, bs = "re") +
                  s(nodeID, bs = "cr", k = 50, m = 2) +
                  s(nodeID, dx, bs = "fs", k = 50, m = 2),
@@ -118,11 +119,67 @@ lunc_groupGS <- bam(dti_fa ~ sex +
                method = "fREML"
 )
 gam.check(lunc_groupGS, rep = 1000)
+summary(lunc_groupGS)
 plot(lunc_groupGS)
 
 plot_lunc_groupGS <- getViz(lunc_groupGS)
 plot(sm(plot_lunc_groupGS, 1))
 plot(sm(plot_lunc_groupGS, 2))
 plot(sm(plot_lunc_groupGS, 3))
+plot(sm(plot_lunc_groupGS, 4))
 
-# split dataframes by dx
+# TODO split dataframes by dx
+
+
+# smooth for group * pscared
+# ti(nodeID, pscared, by = dx, bs = c("cr", "tp"), k = c(50, 10), m = 1)
+lunc_groupGS_pscared <- bam(dti_fa ~ sex +
+    s(dx, bs = "re") +
+    s(subjectID, bs = "re") +
+    s(pscared, k = 10) +
+    s(nodeID, bs = "cr", k = 50, m = 2) +
+    s(nodeID, dx, bs = "fs", k = 50, m = 2) +
+    ti(nodeID, pscared, bs = c("cr", "tp"), k = c(50, 10), m = 2),
+  data = df_tract,
+  family = gaussian(),
+  method = "fREML"
+)
+gam.check(lunc_groupGS_pscared, rep = 1000)
+summary(lunc_groupGS_pscared)
+plot(lunc_groupGS_pscared)
+
+plot_lunc_groupGS_pscared <- getViz(lunc_groupGS_pscared)
+plot(sm(plot_lunc_groupGS_pscared, 1))
+plot(sm(plot_lunc_groupGS_pscared, 2))
+plot(sm(plot_lunc_groupGS_pscared, 3))
+plot(sm(plot_lunc_groupGS_pscared, 4))
+plot(sm(plot_lunc_groupGS_pscared, 5))
+plot(sm(plot_lunc_groupGS_pscared, 6))
+plot(sm(plot_lunc_groupGS_pscared, 7))
+plot(sm(plot_lunc_groupGS_pscared, 8))
+plot(sm(plot_lunc_groupGS_pscared, 9))
+
+
+# ordered dx factors
+# df_tract$dxOF <- factor(df_tract$dx, ordered = T)
+lunc_groupGS_pscared_OF <- bam(dti_fa ~ sex + dx +
+    s(subjectID, bs = "re") +
+    s(nodeID, bs = "cr", k = 50, m = 2) +
+    s(nodeID, dx, bs = "fs", k = 50, m = 2) +
+    ti(nodeID, pscared, by = dxOF, bs = c("cr", "tp"), k = c(50, 3), m = 2) ,
+  data = df_tract,
+  family = gaussian(),
+  method = "fREML"
+)
+gam.check(lunc_groupGS_pscared_OF, rep = 1000)
+summary(lunc_groupGS_pscared_OF)
+plot(lunc_groupGS_pscared_OF)
+
+plot_lunc_groupGS_pscared_OF <- getViz(lunc_groupGS_pscared_OF)
+plot(sm(plot_lunc_groupGS_pscared_OF, 1))
+plot(sm(plot_lunc_groupGS_pscared_OF, 2))
+plot(sm(plot_lunc_groupGS_pscared_OF, 3))
+plot(sm(plot_lunc_groupGS_pscared_OF, 4))
+plot(sm(plot_lunc_groupGS_pscared_OF, 5))
+plot(sm(plot_lunc_groupGS_pscared_OF, 6))
+plot(sm(plot_lunc_groupGS_pscared_OF, 7))
--- code/func5_roiStats.R
+++ code/func5_roiStats.R
@@ -5,7 +5,7 @@ library("ggplot2")
 data_dir <- file_path_as_absolute(paste0(getwd(), "/../data"))
 
 
-# vmPFC Diff LM ----
+# amgL vmPFC LMs ----
 df_amgL_vmpfc <- read.csv(paste0(data_dir, "/df_amgL-lvmPFC.csv"))
 df_amgL_vmpfc$dx <- factor(df_amgL_vmpfc$dx)
 df_amgL_vmpfc <- na.omit(df_amgL_vmpfc)
@@ -42,54 +42,34 @@ for(ind in ind_out){
   }
 }
 
-df_amgL_vmpfc$diff <- df_amgL_vmpfc$SnegLF - df_amgL_vmpfc$SneuLF
-
 hist(df_amgL_vmpfc$SneuLF)
 hist(df_amgL_vmpfc$SnegLF)
 
-fit <- lm(diff ~ pscared*dx, data = df_amgL_vmpfc)
-anova(fit)
-summary(fit)
-plot(fit)
+fit_neg <- lm(SnegLF ~ pscared * dx, data = df_amgL_vmpfc)
+summary(fit_neg)
+anova(fit_neg)
 
-# fit_coef <- summary(fit)$coef
+fit_neu <- lm(SneuLF ~ pscared * dx, data = df_amgL_vmpfc)
+summary(fit_neu)
+anova(fit_neu)
 
-# plot(df_amgL_vmpfc$pscared, df_amgL_vmpfc$diff)
-# abline(a=fit_coef[1],b=fit_coef[2])
-# abline(a=fit_coef[1]+fit_coef[3],b=fit_coef[2]+fit_coef[5])
-# abline(a=fit_coef[1]+fit_coef[4],b=fit_coef[2]+fit_coef[6])
+ggplot(data = df_amgL_vmpfc, aes(x = pscared, y = SnegLF, color = dx)) +
+  geom_point() +
+  geom_ribbon(stat = "smooth", method = "lm", se = T, alpha=0.1,
+              aes(color = NULL, group = factor(dx))) +
+  geom_line(stat = "smooth", method = "lm", alpha = 1) +
+  ggtitle("L. Amg -- vmPFC")
 
-ggplot(data = df_amgL_vmpfc, aes(x = pscared, y = diff, color = dx)) +
-  geom_point() + 
-  geom_ribbon(
-    stat='smooth', method = "lm", se=TRUE, alpha=0.1, 
-    aes(color = NULL, group = factor(dx))) +
-  geom_line(stat='smooth', method = "lm", alpha=1) +
-  ggtitle("L. Amg -- vmPFC") +
-  ylab("Diff: negLF-neuLF")
-
-
-# vmPFC MLM -----
-neg <- lm(cbind(SnegLF, SneuLF) ~ pscared*dx, data = df_amgL_vmpfc)
-anova(neg)
-summary(neg)
-
-df_long <- data.frame(
-  subj = rep(df_amgL_vmpfc$subj, 2),
-  dx = rep(df_amgL_vmpfc$dx, 2),
-  pscared = rep(df_amgL_vmpfc$pscared, 2),
-  beh = rep(c("negLF", "neuLF"), each = dim(df_amgL_vmpfc)[1])
-)
-df_long$coef <- NA
-df_long[which(df_long$beh == "negLF"),]$coef <- df_amgL_vmpfc$SnegLF
-df_long[which(df_long$beh == "neuLF"),]$coef <- df_amgL_vmpfc$SneuLF
-
-df_long %>%
-  group_by(dx) %>%
-  do(broom::tidy(lm(coef ~ pscared*beh)))
+ggplot(data = df_amgL_vmpfc, aes(x = pscared, y = SneuLF, color = dx)) +
+  geom_point() +
+  geom_ribbon(stat = "smooth", method = "lm", se = T, alpha=0.1,
+              aes(color = NULL, group = factor(dx))) +
+  geom_line(stat = "smooth", method = "lm", alpha = 1) +
+  ggtitle("L. Amg -- vmPFC")
 
 
-# dmPFC Diff LM-----
+
+# amgL dmPFC LMs ----
 df_amgL_dmpfc <- read.csv(paste0(data_dir, "/df_amgL-ldmPFC.csv"))
 df_amgL_dmpfc$dx <- factor(df_amgL_dmpfc$dx)
 df_amgL_dmpfc <- na.omit(df_amgL_dmpfc)
@@ -126,26 +106,155 @@ for(ind in ind_out){
   }
 }
 
-df_amgL_dmpfc$diff <- df_amgL_dmpfc$SnegLF - df_amgL_dmpfc$SneuLF
-
 hist(df_amgL_dmpfc$SneuLF)
 hist(df_amgL_dmpfc$SnegLF)
 
-fit <- lm(diff ~ pscared*dx, data = df_amgL_dmpfc)
-anova(fit)
-summary(fit)
-plot(fit)
+fit_neg <- lm(SnegLF ~ pscared * dx, data = df_amgL_dmpfc)
+summary(fit_neg)
+anova(fit_neg)
 
-# fit_coef <- summary(fit)$coef
+fit_neu <- lm(SneuLF ~ pscared * dx, data = df_amgL_dmpfc)
+summary(fit_neu)
+anova(fit_neu)
 
-# plot(df_amgL_dmpfc$pscared, df_amgL_dmpfc$diff)
-# abline(a=fit_coef[1],b=fit_coef[2])
-# abline(a=fit_coef[1]+fit_coef[3],b=fit_coef[2]+fit_coef[5])
-# abline(a=fit_coef[1]+fit_coef[4],b=fit_coef[2]+fit_coef[6])
+ggplot(data = df_amgL_dmpfc, aes(x = pscared, y = SnegLF, color = dx)) +
+  geom_point() +
+  geom_ribbon(stat = "smooth", method = "lm", se = T, alpha=0.1,
+              aes(color = NULL, group = factor(dx))) +
+  geom_line(stat = "smooth", method = "lm", alpha = 1) +
+  ggtitle("L. Amg -- dmPFC")
 
-ggplot(data = df_amgL_dmpfc, aes(x = pscared, y = diff, color = dx)) +
-  geom_point() + 
-  geom_smooth(method = "lm") + 
-  ggtitle("L. Amg -- dmPFC") +
-  ylab("Diff: negLF-neuLF")
-  
+ggplot(data = df_amgL_dmpfc, aes(x = pscared, y = SneuLF, color = dx)) +
+  geom_point() +
+  geom_ribbon(stat = "smooth", method = "lm", se = T, alpha=0.1,
+              aes(color = NULL, group = factor(dx))) +
+  geom_line(stat = "smooth", method = "lm", alpha = 1) +
+  ggtitle("L. Amg -- dmPFC")
+
+
+
+# blaL vmPFC LMs ----
+df_blaL_vmpfc <- read.csv(paste0(data_dir, "/df_blaL-lvmPFC.csv"))
+df_blaL_vmpfc$dx <- factor(df_blaL_vmpfc$dx)
+df_blaL_vmpfc <- na.omit(df_blaL_vmpfc)
+
+# find outliers of SnegLF
+h_iqr <- IQR(df_blaL_vmpfc$SnegLF, na.rm = TRUE)
+h_quant <- quantile(df_blaL_vmpfc$SnegLF, na.rm = TRUE, names = FALSE)
+h_min <- h_quant[2] - (1.5 * h_iqr)
+h_max <- h_quant[4] + (1.5 * h_iqr)
+
+# replace outliers with min/max
+ind_out <- which(df_blaL_vmpfc$SnegLF < h_min | df_blaL_vmpfc$SnegLF > h_max)
+for(ind in ind_out){
+  if(df_blaL_vmpfc[ind,]$SnegLF > h_max){
+    df_blaL_vmpfc[ind,]$SnegLF <- h_max
+  }else if(df_blaL_vmpfc[ind,]$SnegLF < h_min){
+    df_blaL_vmpfc[ind,]$SnegLF <- h_min
+  }
+}
+
+# find outliers of SneuLF
+h_iqr <- IQR(df_blaL_vmpfc$SneuLF, na.rm = TRUE)
+h_quant <- quantile(df_blaL_vmpfc$SneuLF, na.rm = TRUE, names = FALSE)
+h_min <- h_quant[2] - (1.5 * h_iqr)
+h_max <- h_quant[4] + (1.5 * h_iqr)
+
+# replace outliers with min/max
+ind_out <- which(df_blaL_vmpfc$SneuLF < h_min | df_blaL_vmpfc$SneuLF > h_max)
+for(ind in ind_out){
+  if(df_blaL_vmpfc[ind,]$SneuLF > h_max){
+    df_blaL_vmpfc[ind,]$SneuLF <- h_max
+  }else if(df_blaL_vmpfc[ind,]$SneuLF < h_min){
+    df_blaL_vmpfc[ind,]$SneuLF <- h_min
+  }
+}
+
+hist(df_blaL_vmpfc$SneuLF)
+hist(df_blaL_vmpfc$SnegLF)
+
+fit_neg <- lm(SnegLF ~ pscared * dx, data = df_blaL_vmpfc)
+summary(fit_neg)
+anova(fit_neg)
+
+fit_neu <- lm(SneuLF ~ pscared * dx, data = df_blaL_vmpfc)
+summary(fit_neu)
+anova(fit_neu)
+
+ggplot(data = df_blaL_vmpfc, aes(x = pscared, y = SnegLF, color = dx)) +
+  geom_point() +
+  geom_ribbon(stat = "smooth", method = "lm", se = T, alpha=0.1,
+              aes(color = NULL, group = factor(dx))) +
+  geom_line(stat = "smooth", method = "lm", alpha = 1) +
+  ggtitle("L. BLA -- vmPFC")
+
+ggplot(data = df_blaL_vmpfc, aes(x = pscared, y = SneuLF, color = dx)) +
+  geom_point() +
+  geom_ribbon(stat = "smooth", method = "lm", se = T, alpha=0.1,
+              aes(color = NULL, group = factor(dx))) +
+  geom_line(stat = "smooth", method = "lm", alpha = 1) +
+  ggtitle("L. BLA -- vmPFC")
+
+
+
+# blaL dmPFC LMs ----
+df_blaL_dmpfc <- read.csv(paste0(data_dir, "/df_blaL-ldmPFC.csv"))
+df_blaL_dmpfc$dx <- factor(df_blaL_dmpfc$dx)
+df_blaL_dmpfc <- na.omit(df_blaL_dmpfc)
+
+# find outliers of SnegLF
+h_iqr <- IQR(df_blaL_dmpfc$SnegLF, na.rm = TRUE)
+h_quant <- quantile(df_blaL_dmpfc$SnegLF, na.rm = TRUE, names = FALSE)
+h_min <- h_quant[2] - (1.5 * h_iqr)
+h_max <- h_quant[4] + (1.5 * h_iqr)
+
+# replace outliers with min/max
+ind_out <- which(df_blaL_dmpfc$SnegLF < h_min | df_blaL_dmpfc$SnegLF > h_max)
+for(ind in ind_out){
+  if(df_blaL_dmpfc[ind,]$SnegLF > h_max){
+    df_blaL_dmpfc[ind,]$SnegLF <- h_max
+  }else if(df_blaL_dmpfc[ind,]$SnegLF < h_min){
+    df_blaL_dmpfc[ind,]$SnegLF <- h_min
+  }
+}
+
+# find outliers of SneuLF
+h_iqr <- IQR(df_blaL_dmpfc$SneuLF, na.rm = TRUE)
+h_quant <- quantile(df_blaL_dmpfc$SneuLF, na.rm = TRUE, names = FALSE)
+h_min <- h_quant[2] - (1.5 * h_iqr)
+h_max <- h_quant[4] + (1.5 * h_iqr)
+
+# replace outliers with min/max
+ind_out <- which(df_blaL_dmpfc$SneuLF < h_min | df_blaL_dmpfc$SneuLF > h_max)
+for(ind in ind_out){
+  if(df_blaL_dmpfc[ind,]$SneuLF > h_max){
+    df_blaL_dmpfc[ind,]$SneuLF <- h_max
+  }else if(df_blaL_dmpfc[ind,]$SneuLF < h_min){
+    df_blaL_dmpfc[ind,]$SneuLF <- h_min
+  }
+}
+
+hist(df_blaL_dmpfc$SneuLF)
+hist(df_blaL_dmpfc$SnegLF)
+
+fit_neg <- lm(SnegLF ~ pscared * dx, data = df_blaL_dmpfc)
+summary(fit_neg)
+anova(fit_neg)
+
+fit_neu <- lm(SneuLF ~ pscared * dx, data = df_blaL_dmpfc)
+summary(fit_neu)
+anova(fit_neu)
+
+ggplot(data = df_blaL_dmpfc, aes(x = pscared, y = SnegLF, color = dx)) +
+  geom_point() +
+  geom_ribbon(stat = "smooth", method = "lm", se = T, alpha=0.1,
+              aes(color = NULL, group = factor(dx))) +
+  geom_line(stat = "smooth", method = "lm", alpha = 1) +
+  ggtitle("L. BLA -- dmPFC")
+
+ggplot(data = df_blaL_dmpfc, aes(x = pscared, y = SneuLF, color = dx)) +
+  geom_point() +
+  geom_ribbon(stat = "smooth", method = "lm", se = T, alpha=0.1,
+              aes(color = NULL, group = factor(dx))) +
+  geom_line(stat = "smooth", method = "lm", alpha = 1) +
+  ggtitle("L. BLA -- dmPFC")
