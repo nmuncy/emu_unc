5d75265bd956d763cf3268646a85540acd5301f6
Nathan Muncy <nmuncy@fiu.edu>
2022-02-23 18:21:24 -0500

    unpacked difference smooth

--- code/diff4_stats.R
+++ code/diff4_stats.R
@@ -58,10 +58,10 @@ summary(lunc_dxGS)
 plot(lunc_dxGS)
 
 # L. Unc: GI
-lunc_dxGI <- bam(dti_fa ~ sex + 
+lunc_dxGI <- bam(dti_fa ~ sex +
   s(subjectID, bs = "re") +
   s(dx_group, bs = "re") +
-    s(nodeID, bs = "cr", k = 50, m = 2) +
+  s(nodeID, bs = "cr", k = 50, m = 2) +
   s(nodeID, by = dx_group, bs = "cr", k = 50, m = 1),
 data = df_tract,
 family = gaussian(),
@@ -87,8 +87,59 @@ plot(lunc_dxGS_OF)
 summary(lunc_dxGS_OF)
 plot_lunc_dxGS_OF <- getViz(lunc_dxGS_OF)
 plot(sm(plot_lunc_dxGS_OF, 2))
-plot(sm(plot_lunc_dxGS_OF, 3)) +
+plot(sm(plot_lunc_dxGS_OF, 3))
+
+# unpack difference smooth data
+p <- plot(sm(plot_lunc_dxGS_OF, 3)) +
   geom_hline(yintercept = 0)
+p_data <- as.data.frame(p$data$fit)
+
+# find sig nodes
+p_data$lb <- as.numeric(p_data$y - (2 * p_data$se))
+p_data$ub <- as.numeric(p_data$y + (2 * p_data$se))
+sig_nodes <- which(
+  (p_data$y < 0 & p_data$ub < 0) |
+    (p_data$y > 0 & p_data$lb > 0)
+)
+
+# find start, end points
+vec_start <- sig_nodes[1]
+vec_end <- vector()
+y_min <- min(p_data$lb)
+num_nodes <- length(sig_nodes)
+c <- 2
+while (c < num_nodes) {
+  cc <- c + 1
+  if (sig_nodes[cc] > sig_nodes[c] + 1) {
+    vec_end <- append(vec_end, sig_nodes[c])
+    vec_start <- append(vec_start, sig_nodes[cc])
+  }
+  c <- cc
+}
+vec_end <- append(vec_end, sig_nodes[num_nodes])
+
+# make df for drawing rectangles, adjust for 0-index nodeID
+d_rect <- data.frame(
+  x_start = vec_start,
+  x_end = vec_end,
+  y_start = rep(y_min, length(vec_start)),
+  y_end = rep(0, length(vec_start))
+)
+d_rect$x_start <- d_rect$x_start - 1
+d_rect$x_end <- d_rect$x_end - 1
+
+# draw
+p + annotate(
+  "rect",
+  xmin = c(d_rect$x_start),
+  xmax = c(d_rect$x_end),
+  ymin = c(d_rect$y_start),
+  ymax = c(d_rect$y_end),
+  alpha = 0.1,
+  fill = "red"
+) +
+  ggtitle("Difference Smooth, Patient-Control") +
+  ylab("Est. Difference")
 
 
 # R. Unc ----
@@ -105,24 +156,24 @@ descdist(df_tract$dti_fa, discrete = F)
 
 # build gam
 runc_gaus <- bam(dti_fa ~ sex +
-                   s(subjectID, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50),
-                 data = df_tract,
-                 family = gaussian(),
-                 method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(runc_gaus, rep = 1000)
 summary(runc_gaus)
 plot(runc_gaus)
 
 # R. Unc: GS
-runc_dxGS <- bam(dti_fa ~ sex + 
-                   s(subjectID, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50, m = 2) +
-                   s(nodeID, dx_group, bs = "fs", k = 50, m = 2),
-                 data = df_tract,
-                 family = gaussian(),
-                 method = "fREML"
+runc_dxGS <- bam(dti_fa ~ sex +
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50, m = 2) +
+  s(nodeID, dx_group, bs = "fs", k = 50, m = 2),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(runc_dxGS, rep = 1000)
 compareML(runc_gaus, runc_dxGS) # runc_dxGS preferred
@@ -130,14 +181,14 @@ summary(runc_dxGS)
 plot(runc_dxGS)
 
 # R. Unc: GI
-runc_dxGI <- bam(dti_fa ~ sex + 
-                   s(subjectID, bs = "re") +
-                   s(dx_group, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50, m = 2) +
-                   s(nodeID, by = dx_group, bs = "cr", k = 50, m = 1),
-                 data = df_tract,
-                 family = gaussian(),
-                 method = "fREML"
+runc_dxGI <- bam(dti_fa ~ sex +
+  s(subjectID, bs = "re") +
+  s(dx_group, bs = "re") +
+  s(nodeID, bs = "cr", k = 50, m = 2) +
+  s(nodeID, by = dx_group, bs = "cr", k = 50, m = 1),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(runc_dxGI, rep = 1000)
 summary(runc_dxGI)
@@ -146,13 +197,13 @@ compareML(runc_dxGI, runc_dxGS) # runc_dxGS preferred
 
 # R. Unc: GS difference via ordered factors
 df_tract$dx_groupOF <- factor(df_tract$dx_group, ordered = T)
-runc_dxGS_OF <- bam(dti_fa ~ sex + 
-                      s(subjectID, bs = "re") +
-                      s(nodeID, bs = "cr", k = 50, m = 2) +
-                      s(nodeID, by = dx_groupOF, bs = "cr", k = 50, m = 2),
-                    data = df_tract,
-                    family = gaussian(),
-                    method = "fREML"
+runc_dxGS_OF <- bam(dti_fa ~ sex +
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50, m = 2) +
+  s(nodeID, by = dx_groupOF, bs = "cr", k = 50, m = 2),
+data = df_tract,
+family = gaussian(),
+method = "fREML"
 )
 gam.check(runc_dxGS_OF, rep = 1000)
 plot(runc_dxGS_OF)
@@ -179,32 +230,32 @@ ggplot(df_tract, aes(y = dti_fa, x = nodeID)) +
 
 # build gam
 lcgc_beta <- bam(dti_fa ~ sex +
-                   s(subjectID, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50),
-                 data = df_tract,
-                 family = betar(link = "logit"),
-                 method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50),
+data = df_tract,
+family = betar(link = "logit"),
+method = "fREML"
 )
 gam.check(lcgc_beta, rep = 1000)
 
 lcgc_gamma <- bam(dti_fa ~ sex +
-                    s(subjectID, bs = "re") +
-                    s(nodeID, bs = "cr", k = 50),
-                  data = df_tract,
-                  family = Gamma(link = "logit"),
-                  method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50),
+data = df_tract,
+family = Gamma(link = "logit"),
+method = "fREML"
 )
 gam.check(lcgc_gamma, rep = 1000)
 compareML(lcgc_beta, lcgc_gamma)
 
 # L. Cing: GS
-lcgc_dxGS <- bam(dti_fa ~ sex + 
-                   s(subjectID, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50, m = 2) +
-                   s(nodeID, dx_group, bs = "fs", k = 50, m = 2),
-                 data = df_tract,
-                 family = Gamma(link = "logit"),
-                 method = "fREML"
+lcgc_dxGS <- bam(dti_fa ~ sex +
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50, m = 2) +
+  s(nodeID, dx_group, bs = "fs", k = 50, m = 2),
+data = df_tract,
+family = Gamma(link = "logit"),
+method = "fREML"
 )
 gam.check(lcgc_dxGS, rep = 1000)
 compareML(lcgc_gamma, lcgc_dxGS) # almost equal
@@ -212,14 +263,14 @@ summary(lcgc_dxGS)
 plot(lcgc_dxGS)
 
 # L. Cing: GI
-lcgc_dxGI <- bam(dti_fa ~ sex + 
-                   s(subjectID, bs = "re") +
-                   s(dx_group, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50, m = 2) +
-                   s(nodeID, by = dx_group, bs = "cr", k = 50, m = 1),
-                 data = df_tract,
-                 family = Gamma(link = "logit"),
-                 method = "fREML"
+lcgc_dxGI <- bam(dti_fa ~ sex +
+  s(subjectID, bs = "re") +
+  s(dx_group, bs = "re") +
+  s(nodeID, bs = "cr", k = 50, m = 2) +
+  s(nodeID, by = dx_group, bs = "cr", k = 50, m = 1),
+data = df_tract,
+family = Gamma(link = "logit"),
+method = "fREML"
 )
 gam.check(lcgc_dxGI, rep = 1000)
 summary(lcgc_dxGI)
@@ -243,32 +294,32 @@ ggplot(df_tract, aes(y = dti_fa, x = nodeID)) +
 
 # build gam
 rcgc_beta <- bam(dti_fa ~ sex +
-                   s(subjectID, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50),
-                 data = df_tract,
-                 family = betar(link = "logit"),
-                 method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50),
+data = df_tract,
+family = betar(link = "logit"),
+method = "fREML"
 )
 gam.check(rcgc_beta, rep = 1000)
 
 rcgc_gamma <- bam(dti_fa ~ sex +
-                   s(subjectID, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50),
-                 data = df_tract,
-                 family = Gamma(link = "logit"),
-                 method = "fREML"
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50),
+data = df_tract,
+family = Gamma(link = "logit"),
+method = "fREML"
 )
 gam.check(rcgc_gamma, rep = 1000)
 compareML(rcgc_beta, rcgc_gamma)
 
 # R. Cing: GS
-rcgc_dxGS <- bam(dti_fa ~ sex + 
-                   s(subjectID, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50, m = 2) +
-                   s(nodeID, dx_group, bs = "fs", k = 50, m = 2),
-                 data = df_tract,
-                 family = Gamma(link = "logit"),
-                 method = "fREML"
+rcgc_dxGS <- bam(dti_fa ~ sex +
+  s(subjectID, bs = "re") +
+  s(nodeID, bs = "cr", k = 50, m = 2) +
+  s(nodeID, dx_group, bs = "fs", k = 50, m = 2),
+data = df_tract,
+family = Gamma(link = "logit"),
+method = "fREML"
 )
 gam.check(rcgc_dxGS, rep = 1000)
 compareML(rcgc_gamma, rcgc_dxGS) # almost equal
@@ -276,14 +327,14 @@ summary(rcgc_dxGS)
 plot(rcgc_dxGS)
 
 # R. Cing: GI
-rcgc_dxGI <- bam(dti_fa ~ sex + 
-                   s(subjectID, bs = "re") +
-                   s(dx_group, bs = "re") +
-                   s(nodeID, bs = "cr", k = 50, m = 2) +
-                   s(nodeID, by = dx_group, bs = "cr", k = 50, m = 1),
-                 data = df_tract,
-                 family = Gamma(link = "logit"),
-                 method = "fREML"
+rcgc_dxGI <- bam(dti_fa ~ sex +
+  s(subjectID, bs = "re") +
+  s(dx_group, bs = "re") +
+  s(nodeID, bs = "cr", k = 50, m = 2) +
+  s(nodeID, by = dx_group, bs = "cr", k = 50, m = 1),
+data = df_tract,
+family = Gamma(link = "logit"),
+method = "fREML"
 )
 gam.check(rcgc_dxGI, rep = 1000)
 summary(rcgc_dxGI)
